9027b34ddfe12ad12581f02321ece452bacf6406
Hector Sanchez <hsanchez@pressenter.com.ar>
2013-05-24 18:13:00 -0300

    Agregando boton de salir y tecla esc a la ayuda

--- escena_ayuda.py
+++ escena_ayuda.py
@@ -15,21 +15,15 @@ class Ayuda(pilas.escena.Base):
     def __init__(self):
         pilas.escena.Base.__init__(self)
 
-    def iniciar(self): 
+    def iniciar(self):
         pilas.fondos.Fondo('data/ayuda.png')
-        
-        #boton = pilas.actores.Boton()
-
 		
-        #self.volver = pilas.actores.Actor('data/volver.png')
-        #self.volver.x = 0 
-        #self.volver.y = -200
-       # self.volver.click_de_mouse.conectar(self.cuando_se_presione_escape)
-       # volver.click_de_mouse.conectar(self.cuando_se_presione_escape)
-        
+        self.volver = pilas.actores.Boton(x=0, y=-200)
+        self.volver.imagen = 'data/volver.png'
+        self.volver.conectar_presionado(self.cuando_se_presione_escape)
+        self.pulsa_tecla_escape.conectar(self.cuando_se_presione_escape)
 
     def cuando_se_presione_escape(self, *k, **kv):
         "Regresa al menu principal"
         import escena_menu
         pilas.cambiar_escena(escena_menu.EscenaMenu())
-
